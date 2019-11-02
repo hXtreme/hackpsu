323801e8dd66e328bb07131dccf1d079d9ae79c6
Harsh <h.x.dev@outlook.com>
2019-11-02 18:13:47 -0400

    Map page as Index page

--- app/views/main.py
+++ app/views/main.py
@@ -6,11 +6,6 @@ import random
 @app.route('/')
 @app.route('/index')
 def index():
-    return render_template('index.html', title='Home')
-
-
-@app.route('/map')
-def map():
     return render_template('map.html', title='Map')
 
 
