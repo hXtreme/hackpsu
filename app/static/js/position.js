6e8257b20b882c930341843e8b545d3e78665458
Harsh <h.x.dev@outlook.com>
2019-11-03 02:23:31 -0500

    helper functions for position related stuff

--- app/static/js/position.js
+++ app/static/js/position.js
@@ -1,3 +1,13 @@
+
+function centerMap(position) {
+    var center = {
+        lat: position.coords.latitude,
+        lng: position.coords.longitude
+    };
+
+    map.panTo(center);
+}
+
 function getPosition() {
     return {lat: 70, lng: -30};
 }
