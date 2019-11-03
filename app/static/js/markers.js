e89610cd42d5d1ea5e32027f66088b90d60576a3
Restyled.io <commits@restyled.io>
2019-11-03 08:01:43 +0000

    Restyled by clang-format

--- app/static/js/markers.js
+++ app/static/js/markers.js
@@ -1,24 +1,18 @@
 function placeMarker(map, person) {
 
-    var icon_person = '<i class="fas fa-user-md"></i>';
+  var icon_person = '<i class="fas fa-user-md"></i>';
 
-    if(person.type == 'requester')
-    {
-        icon_person = '<i class="fas fa-star-of-life"></i>';
-    }
+  if (person.type == 'requester') {
+    icon_person = '<i class="fas fa-star-of-life"></i>';
+  }
 
-    var marker = new google.maps.Marker({
-            position: person.position,
-            icon: icon_person,
-            map: map
-          });
-
+  var marker = new google.maps.Marker(
+      {position : person.position, icon : icon_person, map : map});
 }
 
-
 function clearMarkers() {
-    for (var i = 0; i < markers.length; i++) {
-        markers[i].setMap(null);
-    }
-    markers = [];
+  for (var i = 0; i < markers.length; i++) {
+    markers[i].setMap(null);
+  }
+  markers = [];
 }
