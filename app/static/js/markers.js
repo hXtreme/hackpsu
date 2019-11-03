67f0229002fcbdd1af250c624730ddf6cc658d98
Arth <arth.dharaskar97@gmail.com>
2019-11-03 03:00:28 -0500

    Updated markers.js

--- app/static/js/markers.js
+++ app/static/js/markers.js
@@ -1,3 +1,21 @@
+function placeMarker(map, person) {
+
+    var icon_person = '<i class="fas fa-user-md"></i>';
+
+    if(person.type == 'requester')
+    {
+        icon_person = '<i class="fas fa-star-of-life"></i>';
+    }
+
+    var marker = new google.maps.Marker({
+            position: person.position,
+            icon: icon_person,
+            map: map
+          });
+
+}
+
+
 function clearMarkers() {
     for (var i = 0; i < markers.length; i++) {
         markers[i].setMap(null);
